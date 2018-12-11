472bd5d4ee09a61e6e687ca99dff44d68cbd6b35
Tochukwu Nwachukwu <truetochukz@gmail.com>
2018-12-11 22:41:24 +0200

    stock directive

---
+++ AngularJS-Up-and-Running/chp11/stock-directive/directive.js
@@ -0,0 +1,15 @@
+angular.module('stockMarketApp')
+       .directive('stockWidget', [function(){
+            return {
+                templateUrl: 'stock.html',
+                restrict: 'AE',
+                scope: {
+                    stockData: '='
+                },
+                link: function($scope, $element, $attr){
+                    $scope.getChange = function(stock) {
+                        return Math.ceil(((stock.price - stock.previous)/ stock.previous) *100 );
+                    };
+                }
+            };
+        }]);
