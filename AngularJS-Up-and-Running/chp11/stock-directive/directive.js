7b1f3649f1a903395e22916a142e7ebfdafe82a2
Tochukwu Nwachukwu <truetochukz@gmail.com>
2018-12-12 23:43:57 +0200

    stock directive

--- AngularJS-Up-and-Running/chp11/stock-directive/directive.js
+++ AngularJS-Up-and-Running/chp11/stock-directive/directive.js
@@ -4,12 +4,26 @@ angular.module('stockMarketApp')
                 templateUrl: 'stock.html',
                 restrict: 'AE',
                 scope: {
-                    stockData: '='
+                    stockData: '=stockData'
                 },
-                link: function($scope, $element, $attr){
+                link: function($scope, $element, $attrs){
                     $scope.getChange = function(stock) {
                         return Math.ceil(((stock.price - stock.previous)/ stock.previous) *100 );
                     };
                 }
             };
         }]);
+
+/*
+ * If the attribute used to pass data to the directive is normalized to the same name as the key of the scope object, you can simplify the mapping.
+ * For example the attribute used in <div stock-widget stock-data="s"></div> widget in index.html to pass data to the directive is 'stock-data'. This
+ * is normalized to 'stockData' by angularJS so to map the data passed to stock-data attribute we can simple do 
+ *  scope: {
+ *      stockData: '='
+ *  }
+ *  instead of
+ *  scope: {
+ *       stockData: '=stockData'
+ *  }
+ * which functions exactly the same, but since the key is same as the expected value after the '='.
+ */
