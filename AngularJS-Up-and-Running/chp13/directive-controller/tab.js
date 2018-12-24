9125b2737f687ebb960fb80d487003c138c90e5f
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2018-12-24 18:18:09 +0200

    extending ngModel directive using third party component

--- AngularJS-Up-and-Running/chp13/directive-controller/tab.js
+++ AngularJS-Up-and-Running/chp13/directive-controller/tab.js
@@ -15,7 +15,7 @@ angular.module('StockMarketApp')
 /**
  * Possible options for the require key of the directive definition object: 
  * require: 'tabs'   => implies that AngularJS should locate the directive tabs on the same element, and throw an error if it’s not found.
- * require: '?tabs'  => mplies that AngularJS should try to locate the directive tabs on the same element, but pass null as the fourth argument to the link function if it isn’t found.
+ * require: '?tabs'  => implies that AngularJS should try to locate the directive tabs on the same element, but pass null as the fourth argument to the link function if it isn’t found.
  * require: '^tabs'  => tells AngularJS that the tabs directive must be present on one of the parent elements (not necessarily the immediate parent).
  * require: '?^tabs' => We can also mix and match these prefixed.
  * require: ['tabs', 'ngModel'] => We can have an array of required directives.
